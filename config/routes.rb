a9881d7495b37fd86acbfa620a1c92bb7c031b5e
Lancai <araraaraiguma@gmail.com>
2020-10-25 12:24:57 +0900

    users show save

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # get 'user/precomplete'
-  
+
+
   ## ユーザー登録のメール送信後に送信完了画面へと遷移
   devise_for :users# , :controllers => { :registrations => "registrations" }
 
   root 'pages#index'
   get 'pages/show'
-  get 'users/show'
+
   resources :users
 
 end
